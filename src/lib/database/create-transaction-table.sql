ce91171f7793c4b69904b4f1fbfe91801783d7c3
HoweiAY <howeiauyeung@gmail.com>
2024-10-15 18:22:33 +0800

    Update database schema for budgets and transactions tables

---
+++ src/lib/database/create-transaction-table.sql
@@ -0,0 +1,21 @@
+-- Create a table for transactions
+create table
+    public.transactions (
+        transaction_id uuid not null default gen_random_uuid (),
+        title text not null,
+        transaction_type character varying not null,
+        category_id smallint null,
+        payer_currency character varying null,
+        recipient_currency character varying null,
+        exchange_rate double precision null,
+        amount bigint not null,
+        payer_id uuid null,
+        recipient_id uuid null,
+        budget_id uuid null,
+        description text null,
+        created_at timestamp with time zone not null default (now() at time zone 'utc'::text),
+        updated_at timestamp with time zone null default (now() at time zone 'utc'::text),
+        constraint transactions_pkey primary key (transaction_id),
+        constraint transactions_payer_id_fkey foreign key (payer_id) references users (user_id) on delete set null,
+        constraint transactions_recipient_id_fkey foreign key (recipient_id) references users (user_id) on delete set null
+    ) tablespace pg_default;
